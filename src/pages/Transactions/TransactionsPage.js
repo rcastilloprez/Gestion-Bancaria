393b7930c4eb646d313ccb459236f4d8d17f7de7
Roberto Carlos Castillo Perez <rcastilloprez@gmail.com>
2026-03-20 12:00:21 -0500

    Agregar funcionalidad para cargar y mostrar transacciones en la página de transacciones

--- src/pages/Transactions/TransactionsPage.js
+++ src/pages/Transactions/TransactionsPage.js
@@ -1,5 +1,8 @@
 // src/pages/Transactions/TransactionsPage.js
 
+import Table from "../../components/Table/Table";
+import TransactionService from "../../services/transactionService";
+
 const TransactionsPage = {
   render: async (container) => {
     container.innerHTML = `
@@ -7,9 +10,37 @@ const TransactionsPage = {
         <div class="page__header">
           <h1>Mis transacciones</h1>
         </div>
+        <div id="accounts-table"></div>
       </section>
     `;
   },
+
+
+  _loadTransactions: async(container) => {
+    const tableContainer = container.querySelector("#accounts-table");
+    tableContainer.innerHTML = '<p class="loading">...cargando</p>';
+
+    try {
+      const transactions = await TransactionService.getAll();
+
+      Table.render(tableContainer, {
+        columns: [
+          {key: 'accountId', label: ''},
+          {key: '', label: ''},
+          {key: '', label: ''},
+          {key: '', label: ''},
+
+        ]
+      })
+
+    } catch (error) {
+      tableContainer.innerHTML = `<p class="error">Error al cargar cuentas: ${error.message}</p>`
+    }
+
+
+  }
+
+
 };
 
 export default TransactionsPage;
